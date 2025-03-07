7330ea54f90e024d27c3eb896555249b4f7f89ae
Dylan Neve <dylanneve1@gmail.com>
2025-03-07 17:04:07 +0000

    Practical2: Seperate mutex locks for while loops

--- Practical2/threads.c
+++ Practical2/threads.c
@@ -18,9 +18,7 @@ pthread_cond_t condQ = PTHREAD_COND_INITIALIZER;
 void *Thread1(void *threadid) {
   printf( "Thread1 Running\n" );
 
-  // Lock the global mutex
   pthread_mutex_lock(&global_mutex);
-
   act4ion_11();
   actio6n_12();
   acti6on_13();
@@ -28,16 +26,18 @@ void *Thread1(void *threadid) {
   actio6n_15();
   acti3on_16();
   acti7on_17();
+  pthread_cond_signal(&condJ); // Signal after acti7on_17 modifies gJ
+  pthread_mutex_unlock(&global_mutex);
+
+
+  pthread_mutex_lock(&global_mutex);
   DISPLAY("Waiting for %s\n","(gJ == 7)");
   while (!((gJ == 7))) {
     pthread_cond_wait(&condJ, &global_mutex);
   };
   puff();
   act1ion_18();
-
-  // Signal to other threads waiting on condJ
   pthread_cond_signal(&condJ);
-  // Unlock the global mutex
   pthread_mutex_unlock(&global_mutex);
 
   pthread_exit(NULL);
@@ -47,9 +47,7 @@ void *Thread1(void *threadid) {
 void *Thread2(void *threadid) {
   printf( "Thread2 Running\n" );
 
-  // Lock the global mutex
   pthread_mutex_lock(&global_mutex);
-
   actio9n_21();
   ac8tion_22();
   actio8n_23();
@@ -57,16 +55,18 @@ void *Thread2(void *threadid) {
   acti6on_25();
   acti4on_26();
   acti6on_27();
+  pthread_cond_signal(&condJ); // Signal after acti6on_27 modifies gJ
+  pthread_mutex_unlock(&global_mutex);
+
+
+  pthread_mutex_lock(&global_mutex);
   DISPLAY("Waiting for %s\n","(gJ == 4)");
   while (!((gJ == 4))) {
     pthread_cond_wait(&condJ, &global_mutex);
   };
   puff();
   act7ion_28();
-
-  // Signal to other threads waiting on condJ
   pthread_cond_signal(&condJ);
-  // Unlock the global mutex
   pthread_mutex_unlock(&global_mutex);
 
   pthread_exit(NULL);
@@ -76,9 +76,7 @@ void *Thread2(void *threadid) {
 void *Thread3(void *threadid) {
   printf( "Thread3 Running\n" );
 
-  // Lock the global mutex
   pthread_mutex_lock(&global_mutex);
-
   acti2on_31();
   act6ion_32();
   act7ion_33();
@@ -86,16 +84,18 @@ void *Thread3(void *threadid) {
   ac4tion_35();
   act8ion_36();
   acti6on_37();
+  pthread_cond_signal(&condJ); // Signal after acti6on_37 modifies gJ
+  pthread_mutex_unlock(&global_mutex);
+
+
+  pthread_mutex_lock(&global_mutex);
   DISPLAY("Waiting for %s\n","(gJ == 11)");
   while (!((gJ == 11))) {
     pthread_cond_wait(&condJ, &global_mutex);
   };
   puff();
   ac9tion_38();
-
-  // Signal to other threads waiting on condJ
   pthread_cond_signal(&condJ);
-  // Unlock the global mutex
   pthread_mutex_unlock(&global_mutex);
 
   pthread_exit(NULL);
@@ -105,9 +105,7 @@ void *Thread3(void *threadid) {
 void *Thread4(void *threadid) {
   printf( "Thread4 Running\n" );
 
-  // Lock the global mutex
   pthread_mutex_lock(&global_mutex);
-
   acti7on_41();
   acti5on_42();
   ac7tion_43();
@@ -115,16 +113,18 @@ void *Thread4(void *threadid) {
   acti7on_45();
   acti4on_46();
   act3ion_47();
+  pthread_cond_signal(&condQ); // Signal after act3ion_47 modifies gQ
+  pthread_mutex_unlock(&global_mutex);
+
+
+  pthread_mutex_lock(&global_mutex);
   DISPLAY("Waiting for %s\n","(gQ < 1)");
   while (!((gQ < 1))) {
     pthread_cond_wait(&condQ, &global_mutex);
   };
   puff();
   act7ion_48();
-
-  // Signal to other threads waiting on condQ
   pthread_cond_signal(&condQ);
-  // Unlock the global mutex
   pthread_mutex_unlock(&global_mutex);
 
   pthread_exit(NULL);
@@ -134,9 +134,7 @@ void *Thread4(void *threadid) {
 void *Thread5(void *threadid) {
   printf( "Thread5 Running\n" );
 
-  // Lock the global mutex
   pthread_mutex_lock(&global_mutex);
-
   actio3n_51();
   ac6tion_52();
   actio4n_53();
@@ -144,19 +142,19 @@ void *Thread5(void *threadid) {
   actio3n_55();
   act6ion_56();
   actio6n_57();
+  pthread_cond_signal(&condQ); // Signal after actio6n_57 modifies gQ
+  pthread_mutex_unlock(&global_mutex);
+
+
+  pthread_mutex_lock(&global_mutex);
   DISPLAY("Waiting for %s\n","(gQ > 1)");
   while (!((gQ > 1))) {
     pthread_cond_wait(&condQ, &global_mutex);
   };
   puff();
   acti8on_58();
-
-  // Signal to other threads waiting on condQ
   pthread_cond_signal(&condQ);
-  // Unlock the global mutex
   pthread_mutex_unlock(&global_mutex);
 
   pthread_exit(NULL);
 }
-
-
